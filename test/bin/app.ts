c5b13781c53358617a58642c476c69519df6bc6b
Derek Slenk <derek.slenk@gmail.com>
2023-09-25 16:58:08 -0400

    Change test name
    My account is being dumb on athena right now

--- test/bin/app.ts
+++ test/bin/app.ts
@@ -4,7 +4,7 @@ import cdk = require('aws-cdk-lib');
 import { TestStack } from '../lib';
 
 const app = new cdk.App();
-new TestStack(app, 'AthenaTestStack', {
+new TestStack(app, 'AthenaWorkgroupTestStack', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
